685d38e7e074c689efb9abb0e130decf331cf4b8
Kamelz <kamel1145@gmail.com>
2018-01-30 19:46:40 +0200

    sql uppercase

--- log.py
+++ log.py
@@ -7,9 +7,9 @@ file = open('output.txt', 'w')
 
 
 file.write("1. What are the most popular three articles of all time? \n\n")
-rows = db.query("""select title ,(select count(*) from log
-where SUBSTRING(path,10) = slug) as cnt from articles
-order by cnt desc limit 3""")
+rows = db.query("""SELECT title ,(SELECT COUNT(*) FROM log
+WHERE SUBSTRING(path,10) = slug) as cnt FROM articles
+ORDER BY cnt DESC LIMIT 3""")
 
 for row in rows:
     file.write('"%s" — %s views \n' % (row[0], row[1]))
@@ -17,9 +17,10 @@ for row in rows:
 file.write("\n-------------------------------------------- \n")
 file.write("2. Who are the most popular article authors of all time? \n\n")
 rows = db.query("""
-select name ,(select count(*) from log where SUBSTRING(path,10) = slug)
-as cnt from authors join articles on articles.author = authors.id order
-by cnt desc""")
+SELECT name ,(SELECT COUNT(*) FROM log WHERE SUBSTRING(path,10) = slug)
+as cnt FROM authors JOIN articles ON articles.author = authors.id ORDER
+BY cnt DESC""")
+
 
 for row in rows:
     file.write('%s — %s views \n' % (row[0], row[1]))
@@ -29,10 +30,10 @@ file.write("\n-------------------------------------------- \n")
 file.write("3. On which days did more than 1% of requests lead to errors?")
 file.write("\n\n")
 
-rows = db.query("""select to_char(time,'Mon dd,YYYY')
-as year,count(*)::decimal /(select count(*)::decimal from log)
-*100 from log where status !='200 OK' group
-by year""")
+rows = db.query("""SELECT to_char(time,'Mon dd,YYYY')
+as year,COUNT(*)::decimal /(SELECT COUNT(*)::decimal FROM log)
+*100 FROM log WHERE status !='200 OK' GROUP
+BY year""")
 
 for row in rows:
     file.write("%s — %s%s errors \n" % (row[0], round(row[1], 2), "%"))
